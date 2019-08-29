b64e9677b8bc8c89733477dbc3d2ab9cc02e5de7
pharesim <pharesim@protonmail.com>
2019-08-29 16:49:28 +0200

    working on compilation with live data

--- src/compilation.py
+++ src/compilation.py
@@ -18,7 +18,25 @@ steem = Steem(nodes=steemd_nodes)
 chain = Blockchain(steem)
 
 def getVotedPosts():
-  posts = db.select('upvotes',['account','user','link','status','title','type'],{'status LIKE':'voted%','vote_time >':"datetime('now','-1 day')"},'account','9999')
-  print(posts)
+  return db.select('upvotes',['account','user','link','slug','status','title','type'],{'status LIKE':'voted%','vote_time >':"datetime('now','-1 day')"},'account','9999')
 
-getVotedPosts()
+def compilation():
+  content = 'Welcome to the daily compilation post of the Curangel project!'+"\n"
+  content += 'Here we highlight the posts picked by our curators, and give you a resource to discover content worth your time, and maybe even your vote!'+"\n\n"
+
+  last_account = ''
+  posts = getVotedPosts()
+  for post in posts:
+    data = steem.get_content(post['user'],post['slug'])
+    print(data)
+    if post['account'] != last_account:
+      last_account = post['account']
+      content += "\n\n"+'*Curator @'+posts['account']+"*\n"
+      content += '| User | Post | Vote weight |'+"\n"
+      content += '| --- | --- | ---: |'+"\n"
+    if post['title'] == '' and post['type'] == 2:
+      post['title'] = 'Comment'
+      vote = post['status'].split('/')[-1]
+    content += '| '+post['user']+' | <a href="'+post['link']+'">'+post['title']+'</a> | '+vote+' |'+"\n"
+
+compilation()

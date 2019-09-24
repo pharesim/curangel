45bf41ee2c7be9fc62d5426624873fb832b365a2
@lemony-cricket <dev@lemony.cricket>
2019-09-24 01:15:50 -0400

    add mana API

---
+++ html/api/lib/account_util.py
@@ -0,0 +1,94 @@
+from .config import config
+from .db_util import AccountDBHelper
+from .errors import CurangelError
+from .steem_util import Steem
+from .rate_limit import Enforcer
+
+
+class AccountError(CurangelError):
+    pass
+
+
+class BadPasswordError(AccountError):
+    def __init__(self, username):
+        self.username = username
+
+    def _fmt(self):
+        return "bad password for account '{username}'"
+
+
+class RoleError(AccountError):
+    def __init__(self, username, role):
+        self.username = username
+        self.role = role
+
+    def _fmt(self):
+        return "user '{username}' did not have required role '{role}'"
+
+
+class Role:
+    def __init__(self, name, user):
+        self.name = name
+        self.user = user
+        self.granted = False
+
+    def __bool__(self):
+        return self.granted
+
+    def __enter__(self):
+        if not self.granted:
+            raise RoleError(self.user, self.name)
+
+    def __exit__(self, typ, val, tb):
+        pass
+
+    def grant(self):
+        self.granted = True
+
+
+class Account:
+    def __init__(self, username):
+        self.adbh = AccountDBHelper(config.db.file)
+        with self.adbh:
+            self.adbh.query_user_id(username)
+        self.name = username
+        self.admin = Role("admin", self.name)
+        self.curator = Role("curator", self.name)
+        self._last_block = -1
+
+    def login(self, user_hash):
+        with self.adbh:
+            real_hash = self.adbh.query_hash(self.name)
+            if real_hash.lower() != user_hash.lower():
+                raise BadPasswordError(self.name)
+            is_admin, is_curator = self.adbh.query_permissions(self.name)
+            if is_admin:
+                self.admin.grant()
+            if is_curator:
+                self.curator.grant()
+
+    def _get_steem(self):
+        try:
+            return self.__steem
+        except AttributeError:
+            self.__steem = Steem()
+            return self.__steem
+
+    def _update_bars(self):
+        head_block = self._get_steem().head_block_number
+        if self._last_block < head_block:
+            enforcer = Enforcer.from_database_user(config.db.file,
+                                                   self.name,
+                                                   head_block)
+            self._stamina = enforcer.stamina
+            self._mana = enforcer.mana
+
+    @property
+    def mana(self):
+        self._update_bars()
+        return self._mana
+
+    @property
+    def stamina(self):
+        self._update_bars()
+        return self._stamina
---
+++ html/api/lib/steem_util.py
@@ -0,0 +1,14 @@
+import steem.steem
+
+DEFAULT_NODES = ['https://anyx.io',
+                 'https://api.steemit.com',
+                 'https://steemd.minnowsupportproject.org']
+
+
+class Steem(steem.steem.Steem):
+    def __init__(self, **kwargs):
+        if "nodes" in kwargs:
+            nodes = kwargs["nodes"]
+        else:
+            nodes = DEFAULT_NODES
+        super().__init__(nodes, **kwargs)

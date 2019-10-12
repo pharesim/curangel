65ff283b9beb92d836c6ca983a12247b2a39c16d
pharesim <pharesim@protonmail.com>
2019-10-13 00:14:47 +0200

    adjust trail value

--- src/downvotes.py
+++ src/downvotes.py
@@ -8,7 +8,7 @@ from steem.steem import Steem
 from steem.blockchain import Blockchain
 
 FULL_VP_RECHARGE_TIME = 432000
-ADDED_VALUE_TRAIL = 400
+ADDED_VALUE_TRAIL = 450
 
 steemd_nodes = [
   'https://anyx.io',

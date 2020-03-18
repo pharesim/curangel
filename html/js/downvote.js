74ef6431a535539336e33cb1295b9d657841e9bf
pharesim <pharesim@protonmail.com>
2020-03-18 17:21:51 +0100

    switch links to steempeak

--- html/js/downvote.js
+++ html/js/downvote.js
@@ -84,7 +84,7 @@ function loadDownvotesTable(downvotes) {
     // User
     newcolumn = document.createElement('td');
     let newlink = document.createElement('a');
-    newlink.setAttribute('href','https://steemit.com/@'+value.user);
+    newlink.setAttribute('href','https://steempeak.com/@'+value.user);
     newlink.setAttribute('target','_blank');
     newcontent = document.createTextNode(value.user);
     newlink.appendChild(newcontent);

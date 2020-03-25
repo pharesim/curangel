48a1c8d6f8547099e93ed674c456dcb823a6a877
pharesim <pharesim@protonmail.com>
2020-03-26 00:01:32 +0100

    blacklist management

--- src/manage_blacklist.py
+++ src/manage_blacklist.py
@@ -1,27 +1,41 @@
 #! /bin/env python3
+from hive.hive import Hive
+from hive.account import Account
+
 from db import DB
 import uuid
+import requests
+
+from pprint import pprint
+import sys
 
 db = DB('curangel.sqlite3')
 
-# spi steem-engine token, list available at https://steem-engine.rocks/tokens/SPI/richlist
-spifile = open("spi.txt")
-spiusers = []
-user = spifile.readline().strip()
-while user != '':
-  spiusers.append(user)
-  user = spifile.readline().strip()
+def manageSPIholders():
+  # spi steem-engine token, list available at https://steem-engine.rocks/tokens/SPI/richlist
+  URL = "https://steem-engine.rocks/tokens/SPI/richlist"
+  richlist = requests.get(url = URL).json()['richlist']
+  spiusers = []
+  for holder in richlist:
+    if float(holder['balance']) > 0:
+      spiusers.append(holder['account'])
 
-print('Number of SPI holders: '+str(len(spiusers)))
+  print('Number of SPI holders: '+str(len(spiusers)))
 
-for user in spiusers:
-  exists = db.select('blacklist',['user'],{'user':user},'user',1)
-  if len(exists) < 1:
-    db.insert('blacklist',{'id':uuid.uuid4().hex,'user':user,'reason':'holder of spi','account':'pharesim'})
-    print(user+' inserted for holding spi')
+  for user in spiusers:
+    exists = db.select('blacklist',['user'],{'user':user},'user',1)
+    if len(exists) < 1:
+      db.insert('blacklist',{'id':uuid.uuid4().hex,'user':user,'reason':'holder of spi','account':'pharesim'})
+      print(user+' inserted for holding spi')
 
-removeOld = db.select('blacklist',['user'],{'reason': 'holder of spi'},'user',9999)
-for user in removeOld:
-  if user['user'] not in spiusers:
-    db.delete('blacklist',{'user':user['user']})
-    print('removed '+user['user']+' because not holding spi any more')
+  removeOld = db.select('blacklist',['user'],{'reason': 'holder of spi'},'user',9999)
+  print('Checking '+str(len(removeOld))+' entries for changes')
+  for user in removeOld:
+    if user['user'] not in spiusers:
+      db.delete('blacklist',{'user':user['user']})
+      print('removed '+user['user']+' because not holding spi any more')
+
+steemcryptosicko = db.select('blacklist',['user'],{'reason': 'part of steemcryptosicko circle'},'user',9999)
+xiguang = db.select('blacklist',['user'],{'reason': 'delegates to xiguang'},'user',9999)
+
+manageSPIholders()
